eb88426039b5c6fae5ebc7e39c89d8e527db04ae
Neil Doppler <neildoppler@Neils-Laptop.local>
2024-07-12 09:55:14 -0500

    addition of fs read write and append files

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,4 @@
 const router = require('express').Router();
-
 const notesRouter = require('./notes');
 
 router.use('/notes', notesRouter);
--- routes/notes.js
+++ routes/notes.js
@@ -1,1 +1,10 @@
 const notes = require('express').Router();
+const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
+
+notes.get('/', (req, res) => {
+    console.info(`${req.method} request received for notes`);
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+  });
+
+
+  module.exports = notes;
